5fbb60f741446766120ae7133ac7fd1d1df275b5
Raphael Gomide <raphael.gomide@almg.gov.br>
2019-06-26 11:28:54 -0300

    iniciando gobarber

---
+++ gobarber/src/app.js
@@ -0,0 +1,27 @@
+//const express = require('express');
+//const routes = require('./routes');
+
+/**
+ * O sucrase permite o estilo de imports moderno
+ */
+import express from 'express';
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+//module.exports = new App().server;
+export default new App().server;
